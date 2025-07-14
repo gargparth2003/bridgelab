908efa6b76ee522b526064239f37d9596e4e5597
gargparth2003 <122970727+gargparth2003@users.noreply.github.com>
2025-07-14 11:33:29 +0530

    Add files via upload

---
+++ IdCard.java
@@ -0,0 +1,8 @@
+public class IdCard {
+    public static void main(String[] args) {
+    System.out.println("Name : Sanyam Bhatia");
+    System.out.println("Roll No . 2210990795");
+    System.out.println("Branch : CSE");
+
+    }
+}
