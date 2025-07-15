794551162584195959032fa697b19d7efe14cd57
gargparth2003 <122970727+gargparth2003@users.noreply.github.com>
2025-07-15 10:25:06 +0530

    Update IdCard.java

--- IdCard.java
+++ IdCard.java
@@ -1,7 +1,7 @@
 public class IdCard {
     public static void main(String[] args) {
-    System.out.println("Name : Sanyam Bhatia");
-    System.out.println("Roll No . 2210990795");
+    System.out.println("Name : Parth Garg");
+    System.out.println("Roll No . 2210992027");
     System.out.println("Branch : CSE");
 
     }
